7b5cc3b89c2166d1957965c8e9083d84b84e56fd
Jesse Salazar <jsn4991@yahoo.com>
2023-07-25 16:01:22 -0500

    initial component update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import InterestResults from "./components/InterestResults";
+import SavingsForm from "./components/SavingsForm";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState(null);
+
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+    // Should be triggered when form is submitted
+    // You might not directly want to bind it to the submit event on the form though...
+  };
+
+  const yearlyData = []; // per-year results
+
+  if (userInput) {
+    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SavingsForm onCalculate={calculateHandler} />
+      {!userInput && <p style={{textAlign: "center"}}>No input calculated yet.</p>}
+
+      {userInput && <InterestResults data={yearlyData} initialInvestment = {userInput['current-savings']}/>}
     </div>
   );
 }
